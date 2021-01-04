d9f577f905d4202063188ad396007f2b3fd16044
Danny Maier <dannymaier@gmail.com>
2021-01-03 21:57:02 -0800

    added script to get the express servers working; they are working

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 // Dependencies
 // ============================
 const express = require("express");
+
+// Sets up the Express App
+// ============================
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static("./app/public"))
+
+console.log("hello world")
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
