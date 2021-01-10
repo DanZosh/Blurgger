29bef92d7e76f1b433897b02a62583b6dd564c5d
Danny Maier <dannymaier@gmail.com>
2021-01-09 19:47:32 -0800

    added get functionality to the orm.js and the burger.js in the models folder. I'm getting an error about needing middlewear to use the app.use() functionality so i have to track that down. Get request still not yet successful

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
-const connection = require('./connection');
+const connection = require('../config/connection');
 
-module.exports = {
+
 //i dont know what the guts for this are, heres from the read me
 
 // * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
@@ -9,24 +9,38 @@ module.exports = {
 // * `insertOne()`
 // * `updateOne()`
 
-selectAll(){
-    connection.query("SELECT * FROM burgers", function(err,res){
-       if (err) throw err;
-       return res;
-    });
-}
+// selectAll(){
+//     connection.query("SELECT * FROM burgers", function(err,res){
+//        if (err) throw err;
+//        return res;
+//     });
+// }
 
-insertOne(data){
-    connection.query("INSERT INTO burgers SET ?",data, function(err,res){
-        if (err) throw err;
-        return res;
-    });
-}
+// insertOne(data){
+//     connection.query("INSERT INTO burgers SET ?",data, function(err,res){
+//         if (err) throw err;
+//         return res;
+//     });
+// }
 
-updateOne(data){
-    connection.query("UPDATE burgers SET ? WHERE ?",data, function(err,res){
-        if (err) throw err;
-        return res;
-    }); 
+// updateOne(data){
+//     connection.query("UPDATE burgers SET ? WHERE ?",data, function(err,res){
+//         if (err) throw err;
+//         return res;
+//     }); 
 
     // * Export the ORM object in `module.exports`.
+
+const orm = {
+    selectAll: function(burgers, callback) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          callback(result);
+        });
+      }
+
+}
+module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,15 @@
-const orm = require("orm");
+const orm = require("../config/orm");
 
 // * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
 
 
-const burgerStuff = function (response){
-    switch(response){
-        case '1':
-            selectAll();
-            break;
-        case '2':
-            insertOne(data);
-            break;
-        case '3':
-            updateOne(data);
-            break;
-        default:
-            connection.end();
+const burgerStuff = {
+    selectAll:function(cb){
+        orm.selectAll("burgers", function(res) {
+            callback(res);
+        });   
     }
+    
 }
 
 // * Export at the end of the `burger.js` file.
