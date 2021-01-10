c129645776320b7f2f3619aa09cbb90cf21bdb61
Danny Maier <dannymaier@gmail.com>
2021-01-09 19:28:37 -0800

    fleshed out the server.js file to be more complete. Added some content to the index.handlebars file to try to get something to actually show. Attempted a first pass at a get request in the burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,14 @@
 const express = require("express");
+var router = express.Router(); //not sure why this is here but its in the cats example
 const burger = require("../models/burger");
+
+
+router.get("/", function(req,res){
+    burger.all(function(data){
+        var handlebarObject = {
+            burgers:data
+        };
+        console.log(handlebarObject);
+        res.render("index", handlebarObject)
+    })
+})
--- server.js
+++ server.js
@@ -5,18 +5,31 @@ const express = require("express");
 // Sets up the Express App
 // ============================
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; // SET THE PORT
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static("./app/public"))
+app.use(express.urlencoded({ extended: true }));// THIS DE-ENCRYPTS THE DATA AND PARSES IT OUT INTO REQ.BODY
+app.use(express.json());//THIS CONVERTS THE PARSED OUT DATA TO JSON FORMAT
+app.use(express.static("./app/public")) //THIS MAKES THE FOLDER PUBLIC AND GIVES THE BROWSER ACCESS TO THE CONTENT
 
 // TEST ROUTE
 app.get("/", function(req, res) {
     res.send("HELLO WORLD");
   });
 console.log("hello world")
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+
+// //SETS UP HANDLEBARS
+// var exphbs = require("express-handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+
+// // IMPORT ROUTES AND GIVE THE SERVER ACCESS TO THEM
+// var routes = require("./controllers/burgers_controller");
+// app.use(routes);
+
+
+//START THE SERVER SO IT CAN BEGIN LISTENING TO CLIENT REQUESTS
+app.listen(PORT, function() { 
+    console.log("App listening on PORT " + PORT);// LOG THE CONSOLE WHEN OUR SERVER HAS OPENED AND PROVIDE THE LINK!
 });
